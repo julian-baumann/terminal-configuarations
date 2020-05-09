5e43e0fec23948f8fd82c59981b2cb7f83dec17c
Julian Baumann <julian-baumann@outlook.com>
2020-05-09 18:07:16 +0200

    Changed Something

---
+++ CommitNew.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+CommitMessage="Changed Something"
+
+if [ $1 ];then
+    CommitMessage=$1
+fi
+
+cp ../.vimrc .
+cp ../.zshrc .
+cp ../.tmux.conf .
+
+git add .
+git commit -m "$CommitMessage"
